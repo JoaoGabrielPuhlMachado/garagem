eb050428e30d300d7419e338c26d1b8c275e0210
João Gabriel <101881866+JoaoGabrielPuhlMachado@users.noreply.github.com>
2023-06-12 15:49:54 -0300

    Update models.py

--- garagem/models.py
+++ garagem/models.py
@@ -34,7 +34,6 @@ class Cor(models.Model):
 
 
 class Veiculo(models.Model):
-    descricao = models.CharField(max_length=50, null=True, blank=True)
     marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
     categoria = models.ForeignKey(
         Categoria, on_delete=models.PROTECT, related_name="veiculos"
@@ -44,6 +43,11 @@ class Veiculo(models.Model):
     preco = models.DecimalField(
         max_digits=10, decimal_places=2, default=0, null=True, blank=True
     )
+    descricao = models.CharField(max_length=50, null=True, blank=True)
+    acessorio = models.ManyToManyField(Acessorio, related_name="veiculos")
 
     def __str__(self):
         return f"{self.marca} {self.categoria} {self.ano} ({self.cor})"
+
+    class Meta:
+        verbose_name_plural = "veículos"
